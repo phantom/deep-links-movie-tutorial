c016aaa2a138537b6d23244bec7cbed029547a79
Brian Friel <briandfriel@gmail.com>
2022-07-21 14:33:35 -0700

    feat(connect): redirect to phantom app

--- App.tsx
+++ App.tsx
@@ -1,10 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-get-random-values";
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import * as Linking from "expo-linking";
+import nacl from "tweetnacl";
+import bs58 from "bs58";
+
+const BASE_URL = "https://phantom.app/ul/v1/";
+
+const onConnectRedirectLink = Linking.createURL("onConnect");
 
 export default function App() {
+  const [dappKeyPair] = useState(nacl.box.keyPair());
+
+  const connect = async () => {
+    const params = new URLSearchParams({
+      dapp_encryption_public_key: bs58.encode(dappKeyPair.publicKey),
+      cluster: "devnet",
+      app_url: "https://phantom.app",
+      redirect_link: onConnectRedirectLink,
+    });
+
+    const myUrl = `${BASE_URL}connect?${params.toString()}`;
+
+    Linking.openURL(myUrl);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Phantom Deeplinking Tutorial</Text>
+      <Button title="Connect" onPress={connect} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +38,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
