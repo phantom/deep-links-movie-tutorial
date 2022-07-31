be6b78778159987ef784f0aac1d015890f2a3533
Brian Friel <briandfriel@gmail.com>
2022-07-30 18:01:56 -0700

    feat: add starter file

--- App.tsx
+++ App.tsx
@@ -3,20 +3,14 @@ import "react-native-url-polyfill/auto";
 import { Buffer } from "buffer";
 global.Buffer = global.Buffer || Buffer;
 import { StatusBar } from "expo-status-bar";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
-import * as Linking from "expo-linking";
-import nacl from "tweetnacl";
-import bs58 from "bs58";
 import {
   clusterApiUrl,
   Connection,
   PublicKey,
   Transaction,
 } from "@solana/web3.js";
-import { decryptPayload } from "./utils/decryptPayload";
-import { encryptPayload } from "./utils/encryptPayload";
-import { buildUrl } from "./utils/buildUrl";
 import MovieList from "./components/MovieList";
 import Button from "./components/Button";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
@@ -26,20 +20,9 @@ import Toast from "react-native-toast-message";
 import { toastConfig } from "./components/ToastConfig";
 import { COLORS } from "./constants";
 
-const onConnectRedirectLink = Linking.createURL("onConnect");
-const onDisconnectRedirectLink = Linking.createURL("onDisconnect");
-const onSignAndSendTransactionRedirectLink = Linking.createURL(
-  "onSignAndSendTransaction"
-);
-
 const connection = new Connection(clusterApiUrl("devnet"));
 
 export default function App() {
-  const [deeplink, setDeepLink] = useState<string>("");
-  const [dappKeyPair] = useState(nacl.box.keyPair());
-
-  const [sharedSecret, setSharedSecret] = useState<Uint8Array>();
-  const [session, setSession] = useState<string>();
   const [phantomWalletPublicKey, setPhantomWalletPublicKey] =
     useState<PublicKey | null>(null);
 
@@ -47,139 +30,14 @@ export default function App() {
 
   const actionSheetRef = useRef<ActionSheet>(null);
 
-  // Initialize our app's deeplinking protocol on app start-up
-  useEffect(() => {
-    const initializeDeeplinks = async () => {
-      const initialUrl = await Linking.getInitialURL();
-      if (initialUrl) {
-        setDeepLink(initialUrl);
-      }
-    };
-    initializeDeeplinks();
-    const listener = Linking.addEventListener("url", handleDeepLink);
-    return () => {
-      listener.remove();
-    };
-  }, []);
-
-  const handleDeepLink = ({ url }: Linking.EventType) => setDeepLink(url);
-
-  // Handle in-bound links
-  useEffect(() => {
-    setSubmitting(false);
-    if (!deeplink) return;
-
-    const url = new URL(deeplink);
-    const params = url.searchParams;
-
-    // Handle an error response from Phantom
-    if (params.get("errorCode")) {
-      const error = Object.fromEntries([...params]);
-      const message =
-        error?.errorMessage ??
-        JSON.stringify(Object.fromEntries([...params]), null, 2);
-      Toast.show({
-        type: "error",
-        text1: "Error",
-        text2: message,
-      });
-      console.log("error: ", message);
-      return;
-    }
-
-    // Handle a `connect` response from Phantom
-    if (/onConnect/.test(url.pathname)) {
-      const sharedSecretDapp = nacl.box.before(
-        bs58.decode(params.get("phantom_encryption_public_key")!),
-        dappKeyPair.secretKey
-      );
-      const connectData = decryptPayload(
-        params.get("data")!,
-        params.get("nonce")!,
-        sharedSecretDapp
-      );
-      setSharedSecret(sharedSecretDapp);
-      setSession(connectData.session);
-      setPhantomWalletPublicKey(new PublicKey(connectData.public_key));
-      console.log(`connected to ${connectData.public_key.toString()}`);
-    }
-
-    // Handle a `disconnect` response from Phantom
-    if (/onDisconnect/.test(url.pathname)) {
-      setPhantomWalletPublicKey(null);
-      console.log("disconnected");
-    }
-
-    // Handle a `signAndSendTransaction` response from Phantom
-    if (/onSignAndSendTransaction/.test(url.pathname)) {
-      actionSheetRef.current?.hide();
-      const signAndSendTransactionData = decryptPayload(
-        params.get("data")!,
-        params.get("nonce")!,
-        sharedSecret
-      );
-      console.log("signAndSendTrasaction: ", signAndSendTransactionData);
-      Toast.show({
-        type: "success",
-        text1: "Review submitted 🎥",
-        text2: signAndSendTransactionData.signature,
-      });
-    }
-  }, [deeplink]);
-
   // Initiate a new connection to Phantom
-  const connect = async () => {
-    const params = new URLSearchParams({
-      dapp_encryption_public_key: bs58.encode(dappKeyPair.publicKey),
-      cluster: "devnet",
-      app_url: "https://phantom.app",
-      redirect_link: onConnectRedirectLink,
-    });
-    const url = buildUrl("connect", params);
-    Linking.openURL(url);
-  };
+  const connect = async () => {};
 
   // Initiate a disconnect from Phantom
-  const disconnect = async () => {
-    const payload = {
-      session,
-    };
-    const [nonce, encryptedPayload] = encryptPayload(payload, sharedSecret);
-    const params = new URLSearchParams({
-      dapp_encryption_public_key: bs58.encode(dappKeyPair.publicKey),
-      nonce: bs58.encode(nonce),
-      redirect_link: onDisconnectRedirectLink,
-      payload: bs58.encode(encryptedPayload),
-    });
-    const url = buildUrl("disconnect", params);
-    Linking.openURL(url);
-  };
+  const disconnect = async () => {};
 
   // Initiate a new transaction via Phantom. We call this in `components/AddReviewSheet.tsx` to send our movie review to the Solana network
-  const signAndSendTransaction = async (transaction: Transaction) => {
-    if (!phantomWalletPublicKey) return;
-    setSubmitting(true);
-    transaction.feePayer = phantomWalletPublicKey;
-    transaction.recentBlockhash = (
-      await connection.getLatestBlockhash()
-    ).blockhash;
-    const serializedTransaction = transaction.serialize({
-      requireAllSignatures: false,
-    });
-    const payload = {
-      session,
-      transaction: bs58.encode(serializedTransaction),
-    };
-    const [nonce, encryptedPayload] = encryptPayload(payload, sharedSecret);
-    const params = new URLSearchParams({
-      dapp_encryption_public_key: bs58.encode(dappKeyPair.publicKey),
-      nonce: bs58.encode(nonce),
-      redirect_link: onSignAndSendTransactionRedirectLink,
-      payload: bs58.encode(encryptedPayload),
-    });
-    const url = buildUrl("signAndSendTransaction", params);
-    Linking.openURL(url);
-  };
+  const signAndSendTransaction = async (transaction: Transaction) => {};
 
   // Open the 'Add a Review' sheet defined in `components/AddReviewSheet.tsx`
   const openAddReviewSheet = () => {
--- utils/buildUrl.ts
+++
@@ -1,4 +0,0 @@
-import { BASE_URL } from "../constants";
-
-export const buildUrl = (path: string, params: URLSearchParams) =>
-  `${BASE_URL}${path}?${params.toString()}`;
